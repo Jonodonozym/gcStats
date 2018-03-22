b9ab90a7b597dd299223b282845d4f147c7f77fb
bakerjaid <jaidencolebaker@gmail.com>
2018-03-22 17:16:47 +1300

    Added missing getHandlerList static method

--- src/jdz/statsTracker/event/AchievementUnlockEvent.java
+++ src/jdz/statsTracker/event/AchievementUnlockEvent.java
@@ -2,6 +2,7 @@
 package jdz.statsTracker.event;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 
 import jdz.bukkitUtils.events.Event;
 import jdz.statsTracker.achievement.Achievement;
@@ -13,4 +14,8 @@ import lombok.EqualsAndHashCode;
 public class AchievementUnlockEvent extends Event {
 	private final Achievement achievement;
 	private final Player player;
+
+	public static HandlerList getHandlerList() {
+		return getHandlers(StatChangeEvent.class);
+	}
 }
